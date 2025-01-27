93cc9c68fe4149f27d44edb175d531ae4a9a363e
herav <herav.dev@gmail.com>
2025-01-26 23:34:27 -0600

    users URLs and routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,11 @@
 import express from "express"
 import { PORT } from "./config"
-
-import { pool, testConnection } from "./DB"
+import UserRuter from "./users/users.routes"
 
 const app = express()
 
 app.use(express.json())
-app.get("/",(_req,res)=>{
-    res.send("Hello World!!! This is catarsis")
-})
+app.get("/",(_req,res)=>{res.send("Hello World!!! This is catarsis")})
+app.use(UserRuter)
 
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`);})
-testConnection(pool)
