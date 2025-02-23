716f6ec66d63889726df36c0fafe7cbc53d11c21
herav <herav.dev@gmail.com>
2025-02-23 01:18:32 -0600

    users endpoint: refator to handle the response within each controller function

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -3,60 +3,65 @@ import { UserModel } from "./users.model.pg"
 import { validatePartialUser, validateUser } from "./users.schema"
 import { catchError } from "../errors.utils"
 
-export const getUsers = async (_req:Request,res:Response) => {
-    const [error,users] = await catchError(UserModel.getUsers())
+export const getUsers = async (_req:Request,res:Response):Promise<void> => {
+    const [error,users] = await catchError(UserModel.getUsers());
     if(error){
-        res.status(500).json({message:"Impossible to get Users"})
         console.error(error); 
+        res.status(500).json({message:"Impossible to get Users"});
+        return;
     }
-    res.status(200).json(users) 
+    res.status(200).json(users);
 }
 
-export const getUser = async (req:Request,res:Response) => {
-    const {name} = req.params
-    const [error,user] = await catchError(UserModel.getUser(name))
+export const getUser = async (req:Request,res:Response): Promise<void> => {
+    const {name} = req.params;
+    const [error,user] = await catchError(UserModel.getUser(name));
     if(error){
-        res.status(404).json({message:"User not found"})
         console.error(error);
+        res.status(404).json({message:"User not found"});
+        return;
     }
-    res.status(200).json(user)
+    res.status(200).json(user);
 }
 
-export const postUser = async (req:Request,res:Response) => {
-    const resultValidation = validateUser(req.body)
+export const postUser = async (req:Request,res:Response): Promise<void> => {
+    const resultValidation = validateUser(req.body);
     if(resultValidation.success){
-        const [error,user] = await catchError(UserModel.postUser({...resultValidation.data,id:""}))
+        const [error,user] = await catchError(UserModel.postUser({...resultValidation.data,id:""}));
         if(error){
-            res.status(500).json({message:"Impossible to save User"})
             console.error(error);
+            res.status(500).json({message:"Impossible to save User"});
+            return;
         }
-        res.status(200).json(user)
+        res.status(200).json(user);
     }
     else{
         res.status(400).json({error:JSON.parse(resultValidation.error.message)});  
     }
 } 
 
-export const deleteUser = async (req:Request,res:Response) => {
-    const {name} = req.params
-    const [error,status] = await catchError(UserModel.deleteUser(name))
+export const deleteUser = async (req:Request,res:Response): Promise<void> => {
+    const {name} = req.params;
+    const [error,status] = await catchError(UserModel.deleteUser(name));
     if(error){
-        res.status(500).json({message:"Impossible to delete User"})
-        console.error(error)
+        console.error(error);
+        res.status(500).json({message:"Impossible to delete User"});
+        return;
     }
-    res.status(200).json({message:status})
+    res.status(200).json({message:status});
 }
 
-export const putUser = async (req:Request,res:Response) => {
-    const {name} = req.params
-    const resultValidation = validatePartialUser(req.body)
+export const putUser = async (req:Request,res:Response): Promise<void> => {
+    const {name} = req.params;
+    const resultValidation = validatePartialUser(req.body);
     if(resultValidation.success){
-        const [error,user] = await catchError(UserModel.putUser(name,resultValidation.data))
+        const [error,user] = await catchError(UserModel.putUser(name,resultValidation.data));
         if(error){
-            res.status(500).json({message:"Impossible to update User"})
             console.error(error);
+            res.status(500).json({message:"Impossible to update User"});
+            return;
         }
-        res.status(200).json(user)
+        res.status(200).json(user);
     }
     else{res.status(400).json({error:JSON.parse(resultValidation.error.message)});}
 }
