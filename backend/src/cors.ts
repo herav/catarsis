70e2660a51264fa1015f26604ca6d7edead3d3e0
herav <herav.dev@gmail.com>
2025-03-12 19:49:23 -0600

    added features to handle CORS

---
+++ backend/src/cors.ts
@@ -0,0 +1,15 @@
+import { Request,Response,NextFunction } from "express";
+
+const ACCEPTED_ORIGINS:string[]=[
+    "http://localhost:3000"
+];
+
+export function corsHandler(req:Request,res:Response,next:NextFunction){
+    const origin = req.header("origin");
+    if(!origin || typeof origin === "string" && ACCEPTED_ORIGINS.includes(origin)){
+        res.header("Access-Control-Allow-Origin",origin);
+        res.header("Access-Control-Allow-Methods","GET,HEAD,POST,PUT,PATCH,DELETE")
+        res.header("Access-Control-Allow-Headers","Content-Type, Authorization")
+    }
+    next();
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { PORT } from "./config"
 import UsersRouter from "./users/users.routes"
+import { corsHandler } from "./cors"
 
 const app = express()
 app.disable("x-powered-by")
 app.use(express.json())
+app.use(corsHandler)
 app.get("/",(_req,res)=>{res.send("Hello World!!! This is catarsis")})
 app.use(UsersRouter)
 
