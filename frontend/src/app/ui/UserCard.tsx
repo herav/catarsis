a5a9324ec6b2b9de3026605ede2646ef6d215160
herav <herav.dev@gmail.com>
2025-04-07 22:21:39 -0500

    Frontend. Component to show very basic user information

--- frontend/src/app/ui/UserCard.tsx
+++ frontend/src/app/ui/UserCard.tsx
@@ -1,33 +1,24 @@
 import "./UserCard.css"
-import { User } from "../lib/definitions"
 
+type USERCARD={
+  name:string,
+  email:string
+  id:string,
+  password:string
+}
 
-export function UserCard({name, userName}:User){
+export function UserCard({name,email,id,password}:USERCARD){
     return (
       <article className="user-card">
-        <header className="user-card-header">
-          <img className="user-card-img" alt="avatar" src={`https://unavatar.io/${userName}`}/>
+        <div className="user-card-header">
+          <img className="user-card-img" alt="avatar" src={`https://unavatar.io/${name}`}/>
           <div className="user-card-info">
-            <strong>{name}</strong>
-            <span className="user-card-info-userName">@{userName}</span>
+            <span className="user-card-info-name">@{name}</span>
+            <strong>{email}</strong>
+            <strong>{id}</strong>
+            <strong>{password}</strong>
           </div>
-        </header>
-        <aside>
-          <button className="user-card-button-follow">Follow</button>
-        </aside>
+        </div>
       </article>
     )
   };
-
-//import { UserCard } from "./ui/UserCard"  
-//<section className="users-list">
-//  <UserCard name="Diana" userName="didi9595"/>
-//  <UserCard name="Vic" userName="herav"/>
-//  <UserCard name="Una Kravets" userName="una"/>
-//</section>
-
-//.users-list{
-//  display: flex;
-//  flex-direction: column;
-//  gap: 8px;
-//}
